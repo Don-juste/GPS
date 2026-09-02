1a322d2fd443d55f49ca112d33d5a1441cbeb63e
HOUANLOKONON Don-juste <houanlokonondekajuste@gmail.com>
2026-09-02 06:33:02 +0200

    ajout

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 import shemas
 from overpass import rechercher_lieux_overpass
-
+import requests
 limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
 app.state.limiter = limiter
@@ -35,3 +35,22 @@ def rechercher_lieux(request:Request,demande: shemas.RechercheLieu):
         "nombre_resultats": len(resultats),
         "lieux": resultats
     }
+    
+@app.get("/test-reseau")
+def test_reseau():
+    resultats = {}
+    
+    sites_a_tester = {
+        "github": "https://api.github.com",
+        "google": "https://www.google.com",
+        "overpass": "https://overpass-api.de/api/interpreter"
+    }
+    
+    for nom, url in sites_a_tester.items():
+        try:
+            reponse = requests.get(url, timeout=10)
+            resultats[nom] = f"OK - status {reponse.status_code}"
+        except Exception as e:
+            resultats[nom] = f"ECHEC - {str(e)}"
+    
+    return resultats    
